c52cf3c769f7723710efa860d754aab0fd50a8d1
Adrian Carriger <mail@adrianandnicole.com>
2017-02-09 15:55:30 -0800

    fix(core): use more specific type

--- src/provide-once.ts
+++ src/provide-once.ts
@@ -1,8 +1,8 @@
-import { Optional, Provider, SkipSelf, Type } from '@angular/core';
+import { Optional, Provider, FactoryProvider, SkipSelf, Type } from '@angular/core';
 import { reflector } from '@angular/core/src/reflection/reflection';
 import { ReflectionCapabilities } from '@angular/core/src/reflection/reflection_capabilities';
 
-export function ProvideOnce<T>(input: Type<T>): Provider {
+export function ProvideOnce<T>(input: Type<T>): FactoryProvider {
   return {
     provide: input,
     deps: [[new Optional(), new SkipSelf(), input], ...reflector.parameters(input)],
