a290582f62b44c7418bfdfa15443801454c15ac3
Adrian Carriger <mail@adrianandnicole.com>
2017-02-09 15:49:53 -0800

    feat(core): add types

--- src/provide-once.ts
+++ src/provide-once.ts
@@ -1,11 +1,11 @@
-import { Optional, SkipSelf } from '@angular/core';
+import { Optional, Provider, SkipSelf, Type } from '@angular/core';
 import { reflector } from '@angular/core/src/reflection/reflection';
 import { ReflectionCapabilities } from '@angular/core/src/reflection/reflection_capabilities';
 
-export function ProvideOnce(input) {
+export function ProvideOnce<T>(input: Type<T>): Provider {
   return {
     provide: input,
     deps: [[new Optional(), new SkipSelf(), input], ...reflector.parameters(input)],
-    useFactory: (parent, ...args) => parent || reflector.factory(input).apply(this, [...args])
+    useFactory: (parent: Type<T>, ...args) => parent || reflector.factory(input).apply(this, [...args])
   };
 }
