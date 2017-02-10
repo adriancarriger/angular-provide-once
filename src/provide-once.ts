4670a4f79857ae897ef44babd2efe8c5db2a918c
Adrian Carriger <mail@adrianandnicole.com>
2017-02-10 14:06:31 -0800

    refactor(core): remove unneeded import

--- src/provide-once.ts
+++ src/provide-once.ts
@@ -1,6 +1,5 @@
 import { Optional, Provider, FactoryProvider, SkipSelf, Type } from '@angular/core';
 import { reflector } from '@angular/core/src/reflection/reflection';
-import { ReflectionCapabilities } from '@angular/core/src/reflection/reflection_capabilities';
 
 export function ProvideOnce<T>(input: Type<T>): FactoryProvider {
   return {
