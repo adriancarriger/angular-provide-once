a696867f4f35b3300b99f109023746360a968b69
Adrian Carriger <mail@adrianandnicole.com>
2017-02-11 19:11:01 -0800

    chore(build): allow publishing dist to Github

--- webpack.config.js
+++ webpack.config.js
@@ -59,14 +59,6 @@ module.exports = {
                     failOnHint: false
                 }
             }
-        }),
-
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: function (module) {
-                // this assumes your vendor imports exist in the node_modules directory
-                return module.context && module.context.indexOf('node_modules') !== -1;
-            }
         })
     ]
 };
